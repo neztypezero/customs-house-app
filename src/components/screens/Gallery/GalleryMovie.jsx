28fdc0cfda18dc9c9eaaacccae9640ed71c33888
David Nesbitt <neztypezero@me.com>
2021-08-21 14:37:27 -0700

    set movie urls to neilson digital

--- src/components/screens/Gallery/GalleryMovie.jsx
+++ src/components/screens/Gallery/GalleryMovie.jsx
@@ -4,7 +4,7 @@ function GalleryMovie(props) {
 	return (
 		<Link className="movie-link" to="/gallery">
 			<video id="gallerymovie" width="1920" height="1080" loop="loop" autoPlay="autoPlay" muted="muted">
-				<source src="/movie/CH_Video_2020.mp4" type="video/mp4" />
+				<source src="https://neilsondigital1.com/ch_touch/movie/CH_Video_2017.mp4" type="video/mp4" />
 			</video>
 		</Link>
 	);
